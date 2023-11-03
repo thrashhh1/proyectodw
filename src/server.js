016f3a8d335dffef826d51636dfeea61d3b09be8
thrashhh1 <benjatejoh@gmail.com>
2023-11-03 00:06:25 -0300

    Versión 1.1: Cambios realizados

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const path = require('path');
 
 // Initializations
@@ -7,22 +8,26 @@ const app = express();
 // Settings
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
 
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(express.urlencoded({extended: false}));
-
-
 
 // Global Variables
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('hola mundo');
+    res.render('index');
 })
 
 //Static Files
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 module.exports = app;
