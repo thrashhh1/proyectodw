8fe40f6a46b6b1a9bad59da90bc226609f2d550a
thrashhh1 <benjatejoh@gmail.com>
2023-11-03 16:13:21 -0300

    Version 1.3: creacion de rutas

--- src/server.js
+++ src/server.js
@@ -23,9 +23,7 @@ app.use(express.urlencoded({extended: false}));
 // Global Variables
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('index');
-})
+app.use(require('./routes/index.routes'));
 
 //Static Files
 app.use(express.static(path.join(__dirname, 'public')));
